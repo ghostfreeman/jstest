e8e8e159b66314d77d2b8930258975fa346e4716
Cameron Kilgore <ghostfreeman@gmail.com>
2014-07-07 11:43:36 -0400

    Cleaned up unneeded comment blocks, TODOs, and bad language

--- js/RestModel.js
+++ js/RestModel.js
@@ -7,26 +7,17 @@
  * @constructor
  */
 function RestModel(uri) { 
-	//If I don't put these properties somewhere, i'll lose my damn mind
 	this.uri = uri;
 
 	console.log("RestModel object created");
 }
 
-// Methods
 /**
  * Defines a uri for the RestModel Object to work with.
  * @param {string} uri - a valid uri reference 
  * @returns {Boolean} 
  */
 RestModel.prototype.assignObjectUri = function(uri) {
-	// Validation - String
-
-	// TODO Validation - URI elements
-	// At the most, not having a valid Localized IETF or ICANN gTLD
-	// Will throw this out the door.
-
-	// Once validated, assign to field
 	this.uri = uri;
 }
 
@@ -46,9 +37,6 @@ RestModel.prototype.ajax = function() {
 			window.alert("Fatal error! "+errorThrown);
 		},
 	});
-
-	/* $.each(parsedObj, function(i) {
-		intModel.generateObjectByViewType($(this),"action");
-	}); */
+	
 	return parsedObj;
 }
--- js/main.js
+++ js/main.js
@@ -3,27 +3,6 @@
  */
 
 $(document).ready(function() {
-	//var ui = new InterfaceModel();
-	/* var rest = new RestModel("");
-
-	rest.assignObjectUri("http://ec2-54-81-14-120.compute-1.amazonaws.com/api/dummyview");
-	object = rest.ajax();
-
-	// Load page elements
-	$.each(object, function(i) {
-
-		switch($(this)[0].viewType) {
-			case "button" :
-				createButton($(this), "actions");
-				break;
-			case "editBox" :
-				createEditBox($(this), "actions");
-				break;
-			default:
-				console.log("Invalid object initialized");
-				break;
-		}
-	}); */
 	loadAjaxObjects("actions");
 });
 
